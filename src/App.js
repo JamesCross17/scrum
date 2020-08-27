7143660c985f51eae767e956f11c0b5fb01c0d83
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-27 08:02:01 +0000

    Deploy JamesCross17/scrum to github.com/JamesCross17/scrum.git:gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react'
-import './App.css'
-import { Layout } from 'antd'
-import AddMeetingModal from './containers/modals/AddMeetingModal'
-import HeaderMenu from './HeaderMenu'
-import Meetings from './containers/Meetings'
-
-const { Header, Content } = Layout
-
-function App () {
-    return (
-      <Layout>
-          <Header>
-              <HeaderMenu/>
-          </Header>
-          <Content>
-              <AddMeetingModal/>
-              <Meetings/>
-          </Content>
-      </Layout>
-    )
+import React, { useEffect } from 'react'
+import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css';
+function App() {
+    useEffect(()=>{
+        fetch('/users')
+        .then(res => res.json())
+        .then(users => console.log('---',users ));
+    })
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
